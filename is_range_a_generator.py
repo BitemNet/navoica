326662803ace32870b3d8fceaedfa818f304be36
Marek Bladowski <bitemnet@gmail.com>
2024-06-17 17:14:10 +0200

    Add is_range_a_generator.py file

---
+++ is_range_a_generator.py
@@ -0,0 +1,28 @@
+"""
+Is range a generator?
+Here is answer: https://treyhunner.com/2018/02/python-range-is-not-an-iterator/
+"...And unlike iterators, you can ask them whether they contain things without changing their state:...
+>>> numbers = range(1_000_000)
+>>> squares = (n**2 for n in numbers)
+
+>>> 0 in numbers
+True
+>>> 0 in numbers
+True
+>>> 0 in squares
+True
+>>> 0 in squares
+False
+
+"
+"""
+
+
+r = range(1, 4)
+for i in r:
+    for j in r:
+        for k in r:
+            # print(i, j, k, r)
+            print(i * j * k, end=";")
+        print(end=" ")
+    print()
